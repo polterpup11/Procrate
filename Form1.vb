19db80717d8ceb2c477fd8241e246a4ad82a99db
polterpup11 <89555773+polterpup11@users.noreply.github.com>
2022-10-24 00:31:40 -0400

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub CloseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub RunToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RunToolStripMenuItem.Click
+        ListBox1.Items.Clear()
+        ListBox1.Items.AddRange(TextBox1.Lines)
+        For i = 0 To ListBox1.Items.Count - 1
+            Dim teststring As String = ListBox1.Items.Item(i)
+            Dim testarray() As String = Split(teststring)
+            If testarray(0) = "print" Then
+                MsgBox(testarray(1))
+
+            End If
+
+            If testarray(0) = "beep" Then
+                Beep()
+            End If
+            If testarray(0) = "exit" Then
+                End
+            End If
+        Next
+    End Sub
+End Class
